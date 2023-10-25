46b6106c30b820396c1fd3169754cdad62a0553f
yamanidev <elamine.yamani@gmail.com>
2023-10-25 20:51:50 +0100

    Added users table to init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE fastify_auth_jwt;
+
+\c fastify_auth_jwt;
+
+CREATE TABLE users(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(60) NOT NULL
+);
